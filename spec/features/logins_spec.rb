949ae14f69cd2512846a5176ae35a51dc67783bc
cminnerath <cminnerath@gmail.com>
2015-12-19 18:47:53 -0700

    add landing page with logo, image and mission statement

---
+++ spec/features/logins_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "User can navigate to landing page", type: :feature do
+  scenario "successfully" do
+    visit root_path
+
+    expect(page).to have_content "Warrior Status"
+    expect(page).to have_css  "img[src*='rottingbuffalo']"
+    expect(page).to have_content "Mission Statement"
+  end
+end
