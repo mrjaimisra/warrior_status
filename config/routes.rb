027a86e7a63bf1211ae9abf4555622c1fd52a01a
cminnerath <cminnerath@gmail.com>
2015-12-20 19:57:16 -0700

    Admin can logout
    * Add route '/logout' for sessions#destroy
    * Change control panel logout button to #link_to
    * Add sessions#destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get  "/admin",     to: "sessions#new"
   post "/admin",     to: "sessions#create"
   get  "/dashboard", to: "admin#show", as: :dashboard
+  delete "/logout",  to: "sessions#destroy"
 end
