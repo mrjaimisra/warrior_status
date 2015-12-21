8046a3db152c291f1a156c37e9ddfac742cb2e0a
mrjaimisra <misrajai01@gmail.com>
2015-12-20 19:25:21 -0700

    Admin can see control panel sliding menu
    Add  function to application.js
    Add layouts/_control_panel.html.erb partial
    Add #current_admin and #authorize! helper methods
    Set before_action :authorize! for AdminController
    Rename default links to be more relevant

--- spec/features/admin_logs_in_spec.rb
+++ spec/features/admin_logs_in_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 RSpec.feature "Admin can log in", type: :feature do
   let!(:admin) { Admin.create(username: "Admin", password: "password") }
 
+  before do
+    login
+  end
+
   scenario "successfully" do
-
-    visit admin_path
-    fill_in "Username", with: admin.username
-    fill_in "Password", with: admin.password
-    click_on "Login"
-
-    expect(current_path).to eq dashboard_path
-    expect(page).to have_content "Admin Dashboard"
+    expect(current_path).to eq root_path
+    expect(page).to have_content "Edit Profile"
+    expect(page).to have_content "Logout"
   end
 end
