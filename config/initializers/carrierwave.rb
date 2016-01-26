243f617309e514c5bcb431b6379f2378888193b5
mrjaimisra <misrajai01@gmail.com>
2016-01-25 21:19:03 -0700

    Bug - travis-ci build failing due to fog access keys errorchange config.fog_public to false

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,5 +6,5 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
   }
   config.fog_directory   = 'warrior-status'
-  config.fog_public      = true
+  config.fog_public      = false
 end
