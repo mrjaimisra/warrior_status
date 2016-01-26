e53334110426a5d7ab90b37c0ae17a5da59c11f8
mrjaimisra <misrajai01@gmail.com>
2016-01-25 22:48:24 -0700

    bug: travis-ci not recognizing AWS ENV variables - add AWS_S3_BUCKET as ENV VAR

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,6 @@ if Rails.env.production?
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     }
-    config.fog_directory   = 'warrior-status'
+    config.fog_directory   = 'ENV["AWS_S3_BUCKET"]'
   end
 end
